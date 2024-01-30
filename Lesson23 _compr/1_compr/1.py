8220d5ba9f05e918d94e9fbb011bc2802dae34f2
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:10:39 +0400

    Create 1.py

---
+++ Lesson23 _compr/1_compr/1.py
@@ -0,0 +1,4 @@
+#1.Given a list of words, create a list of their lengths.
+object_1=['Hello World'],['A','B','B','A'],['laptop'],[1,2,3,4,5,6,7,8,9]
+len_comp=[len(x) for x in object_1]
+print(len_comp)
