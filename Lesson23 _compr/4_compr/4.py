6591e60b3926ba06f51b0757e8843904c2b7755b
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:10:54 +0400

    Create 4.py

---
+++ Lesson23 _compr/4_compr/4.py
@@ -0,0 +1,4 @@
+#Given a string, create a dictionary where keys are characters, and values are their ASCII values.
+string_ASCII='krikor'
+dic_ASCII={x:ord(x) for x in string_ASCII}
+print(dic_ASCII)
