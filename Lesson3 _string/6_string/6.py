7fb2f6230368ab9ffd5716155f43f76d44f8a2ac
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:00:31 +0400

    Create 6.py

---
+++ Lesson3 _string/6_string/6.py
@@ -0,0 +1,5 @@
+#6.Write a Python function to get a string made of its first three characters of a specified string. If the length of the string is less than 3 then return the original string.
+string_1="fly"
+print(string_1[:3])
+string_2="bird"
+print(string_2[:3])
