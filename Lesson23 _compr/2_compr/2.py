dadebba00075c355d506b25341cd130c7bada463
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:10:46 +0400

    Create 2.py

---
+++ Lesson23 _compr/2_compr/2.py
@@ -0,0 +1,5 @@
+#2.Given a string, create a dictionary where keys are characters and values are their frequencies in the string.
+simbols="abrakadabra alphabet unreal laptop"
+a=0
+sDir={x:a for x in simbols if x==x simbols.count(i)}
+print(sDir)
