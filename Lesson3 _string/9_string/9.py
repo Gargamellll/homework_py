fbc6a4ab40d1cc521a78035114d609e42daa6edc
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:00:42 +0400

    Create 9.py

---
+++ Lesson3 _string/9_string/9.py
@@ -0,0 +1,7 @@
+#9.Append new string in the middle of a given (even number of characters) string
+string_1='homeland'
+a=len(string_1)
+string_2='new'
+print(string_1[:4] + string_2 + string_1[-4:])
+
+
