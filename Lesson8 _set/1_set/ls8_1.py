26c809ffdae36842bdc12866afa39b1db1bf2e22
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:05:06 +0400

    Create ls8_1.py

---
+++ Lesson8 _set/1_set/ls8_1.py
@@ -0,0 +1,4 @@
+#1.Write a Python function that takes two sets as input and returns a new set containing elements that are present in both input sets.
+set_1=set('marshall')
+set_2=set('mirtilli')
+print(set_1|set_2)
