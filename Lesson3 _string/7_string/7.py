8abbebccf8808a74cc92028dd4a2d74b4a6a3341
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:00:35 +0400

    Create 7.py

---
+++ Lesson3 _string/7_string/7.py
@@ -0,0 +1,5 @@
+#7.Write a Python program to print the floating numbers upto 2 decimal places (number must be not greater than 10)
+number_1=3.7584936494
+print(round(number_1,2))
+number_2=56.8393028548340
+print(round(number_2,2))
