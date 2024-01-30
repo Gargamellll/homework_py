cadfd129c0d7e245a78dadbf2c83e5b37141db7c
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 16:59:55 +0400

    Create 3.py

---
+++ Lesson4 _base/3_base/3.py
@@ -0,0 +1,6 @@
+#3.Input two integers and print out their sum. Preserve the exact format from the examples (e.g. the output should contain exactly three lines)
+a=int(input('Choose your mumber' ))
+b=int(input('Choose your number' ))
+z=a+b
+print(z)
+
