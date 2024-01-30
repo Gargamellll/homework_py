9d9a0635ec90bbf13abbdef7022717f8f2d175c6
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:05:15 +0400

    Create ls8_4.py

---
+++ Lesson8 _set/4_set/ls8_4.py
@@ -0,0 +1,5 @@
+#4.Write a Python function that takes two sets as input and returns a new set containing
+#elements that are present in either of the input sets, but not in both.
+set_1=set('bottle')
+set_2=set('holder')
+print(set_1^set_2)
