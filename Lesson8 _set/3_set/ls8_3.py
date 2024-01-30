6e6070ad4ea840f73b114fcba014394c51894974
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:05:12 +0400

    Create ls8_3.py

---
+++ Lesson8 _set/3_set/ls8_3.py
@@ -0,0 +1,5 @@
+#3.Write a Python function that takes two sets as input and returns a new set containing
+#elements that are present in the first set but not in the second set.
+set_1=set('giggle')
+set_2=set('snigger')
+print(set_1 - set_2)
