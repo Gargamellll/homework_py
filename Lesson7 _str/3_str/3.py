ec1624e3bca095b5867b4991dd431498e47051a5
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:04:28 +0400

    Create 3.py

---
+++ Lesson7 _str/3_str/3.py
@@ -0,0 +1,16 @@
+# Write a program to check if two strings are balanced. For example, strings s1 and
+# s2 are balanced if all the characters in the s1 are present in s2. The character’s
+# position doesn’t matter
+
+str_1="PY"
+str_2= "PYnative"
+answer=True
+for x in str_1:
+    if x in str_2:
+        pass
+        
+    else:
+        answer=False    
+  
+
+print(answer)        
