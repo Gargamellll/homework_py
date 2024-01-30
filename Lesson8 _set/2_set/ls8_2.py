9c697294e69d6945d2575b868f099126729db2ae
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:05:09 +0400

    Create ls8_2.py

---
+++ Lesson8 _set/2_set/ls8_2.py
@@ -0,0 +1,5 @@
+#2.Write a Python function that takes two sets as input and returns a new set containing all
+#unique elements from both input sets.
+set_1=set('special')
+set_2=set('specialized')
+print(set_1&set_2)
