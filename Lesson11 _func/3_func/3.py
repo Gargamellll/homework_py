12e0b9ac42cab729158c0f2de0454126c0f248a3
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:43:27 +0400

    Create 3.py

---
+++ Lesson11 _func/3_func/3.py
@@ -0,0 +1,8 @@
+#3.Write a Python function to calculate the factorial of a number (a non-negative
+# integer). The function accepts the number as an argument.
+
+import math
+def factorial(number):
+    return math.factorial(number)
+
+print(factorial(5))    
