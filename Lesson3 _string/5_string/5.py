f69fb79832ec0b96a2c2b13b1274f776c406dc6f
Gargamellll <zuka.poltora12@gmail.com>
2024-01-30 17:00:28 +0400

    Create 5.py

---
+++ Lesson3 _string/5_string/5.py
@@ -0,0 +1,5 @@
+#5.Write a Python function to get a string made of 4 copies of the last two characters of a specified string (length must be at least 2).
+word_1="saccaria"
+print(word_1[:2] * 4)
+word_2="flower"
+print(word_2[-2:] * 4)
